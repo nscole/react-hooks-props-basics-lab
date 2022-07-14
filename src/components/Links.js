3849ff8794c662e00a8012c17c5c707fc1007d50
Naomi Cole <naomisarahcole@gmail.com>
2022-07-14 14:19:47 +1000

    passing 6 tests 8 to go

---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Links(props) {
+  return (
+    <div id="">
+     
+    </div>
+  );
+}
+
+export default Links;
--- src/data/user.js
+++ src/data/user.js
@@ -1,5 +1,5 @@
 const user = {
-  name: "Liza",
+  username: "Liza",
   city: "New York",
   bio: "I made this!",
   color: "firebrick",
